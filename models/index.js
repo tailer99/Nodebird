296e537db5b1d1d0e54f9138a913612080e19012
Daniel Pyo <tailer9999@gmail.com>
2023-02-24 10:38:22 +0900

    current

---
+++ models/index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const path = require("path");
+const Sequelize = require("sequelize");
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.json")[env];
+
+const User = require("./user");
+const Post = require("./post");
+const Hashtag = require("./hashtag");
+const db = {};
+
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+User.init(sequelize);
+Post.init(sequelize);
+Hashtag.init(sequelize);
+
+db.sequelize = sequelize;
+db.User = User;
+db.Post = Post;
+db.Hashtag = Hashtag;
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+module.exports = db;
---
+++ routes/page.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
+
+const router = express.Router();
+
+router.use((req, res, next) => {
+  res.locals.user = req.user;
+  res.locals.followerCount = 0;
+  res.locals.followingCount = 0;
+  res.locals.followerIDList = [];
+  next();
+});
+
+router.get("/profile", isLoggedIn, (req, res) => {
+  res.render("profile", { title: "내 정보 - nodebird " });
+});
+
+router.get("/join", isNotLoggedIn, (req, res) => {
+  res.render("join", { title: "회원가입 -nodebird" });
+});
+
+router.get("/", (req, res, next) => {
+  const twits = [];
+  res.render("main", { title: "NodeBird", twits });
+});
+
+module.exports = router;
